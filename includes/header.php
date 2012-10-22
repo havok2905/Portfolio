edd115c7e78882791ac087e8964bea72706a3a78
mclean.cw <havok2905@gmail.com>
2012-10-22 19:05:16 -0400

    made some content edits to the comp and added header.php

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Christopher McLean, Web Development and Design</title>
+<meta name="description" content="Christopher McLean's Portfolio Website"/>
+<meta name="keywords" content="Web Design, Web Development, JavaScript, CSS, HTML, PHP"/>
+<meta name="author" content="Christopher McLean"/>
+</head>
+<body>
+	<h1><span>Christopher McLean; Web Developer and Designer </span><img src="#" alt="logo"/></h1>
+	<ul>
+		<li>phone: 254-424-4921</li>
+		<li>email: mclean.webdev@gmail.com</li>
+	</ul>
+	<ul>
+		<li><span>LinkedIn </span><img src="#" alt="linkedIn"/></li>
+		<li><span>Git Hub </span><img src="#" alt="Git Hub"/></li>
+		<li><span>Facebook </span><img src="#" alt="Facebook"/></li>
+	</ul>
+	<ul>
+		<li><a href="index.php">home</a></li>
+		<li><a href="me.php">me</a></li>
+		<li><a href="resume.php">resume</a></li>
+		<li><a href="blog.php">blog</a></li>
+		<li><a href="work.php">work</a></li>
+	</ul>
