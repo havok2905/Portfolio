445d07260d3349d3870c5d007bb6722c83ee3093
mclean.cw <havok2905@gmail.com>
2012-12-17 15:30:44 -0600

    finished adding static and dummy markup content sans file paths, ids, and classes

--- blog.php
+++ blog.php
@@ -4,7 +4,92 @@
 	require_once("includes/layout.php");
 	echo Layout::header("Blog");
 ?>
-
+<div>
+	<h3>Topics</h3>
+	<ul>
+		<li><a href="">Programming</a></li>
+		<li><a href="">Graphic Design</a></li>
+		<li><a href="">Technology</a></li>
+		<li><a href="">Interactivity</a></li>
+		<li><a href="">miscellaneous</a></li>
+	</ul>
+</div>
+<div>
+	<h3><a href="">Lorem Ipsum Dolor</a></h3>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac lorem non lectus varius vulputate. 
+		Donec eleifend porta pretium. Pellentesque eu purus non lorem sagittis fringilla vel ut arcu. Ut fermentum 
+		vehicula facilisis. Aliquam bibendum ultricies ipsum, porttitor hendrerit justo sollicitudin vitae. 
+		Curabitur accumsan euismod arcu vitae volutpat. Class aptent taciti sociosqu ad litora torquent per conubia 
+		nostra, per inceptos himenaeos. Mauris nunc sapien, varius quis mattis non, condimentum suscipit massa. 
+		Nunc a nulla ut orci vestibulum suscipit. Pellentesque habitant morbi tristique senectus et netus et 
+		malesuada fames ac turpis egestas.
+	</p>
+	<ul>
+		<li><a href="">Lorem</a></li>
+		<li><a href="">Ipsum</a></li>
+		<li><a href="">Dolor</a></li>
+		<li><a href="">Sit</a></li>
+		<li><a href="">Amet</a></li>
+	</ul>
+</div>
+<div>
+	<h3><a href="">Lorem Ipsum Dolor</a></h3>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac lorem non lectus varius vulputate. 
+		Donec eleifend porta pretium. Pellentesque eu purus non lorem sagittis fringilla vel ut arcu. Ut fermentum 
+		vehicula facilisis. Aliquam bibendum ultricies ipsum, porttitor hendrerit justo sollicitudin vitae. 
+		Curabitur accumsan euismod arcu vitae volutpat. Class aptent taciti sociosqu ad litora torquent per conubia 
+		nostra, per inceptos himenaeos. Mauris nunc sapien, varius quis mattis non, condimentum suscipit massa. 
+		Nunc a nulla ut orci vestibulum suscipit. Pellentesque habitant morbi tristique senectus et netus et 
+		malesuada fames ac turpis egestas.
+	</p>
+	<ul>
+		<li><a href="">Lorem</a></li>
+		<li><a href="">Ipsum</a></li>
+		<li><a href="">Dolor</a></li>
+		<li><a href="">Sit</a></li>
+		<li><a href="">Amet</a></li>
+	</ul>
+</div>
+<div>
+	<h3><a href="">Lorem Ipsum Dolor</a></h3>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac lorem non lectus varius vulputate. 
+		Donec eleifend porta pretium. Pellentesque eu purus non lorem sagittis fringilla vel ut arcu. Ut fermentum 
+		vehicula facilisis. Aliquam bibendum ultricies ipsum, porttitor hendrerit justo sollicitudin vitae. 
+		Curabitur accumsan euismod arcu vitae volutpat. Class aptent taciti sociosqu ad litora torquent per conubia 
+		nostra, per inceptos himenaeos. Mauris nunc sapien, varius quis mattis non, condimentum suscipit massa. 
+		Nunc a nulla ut orci vestibulum suscipit. Pellentesque habitant morbi tristique senectus et netus et 
+		malesuada fames ac turpis egestas.
+	</p>
+	<ul>
+		<li><a href="">Lorem</a></li>
+		<li><a href="">Ipsum</a></li>
+		<li><a href="">Dolor</a></li>
+		<li><a href="">Sit</a></li>
+		<li><a href="">Amet</a></li>
+	</ul>
+</div>
+<div>
+	<h3><a href="">Lorem Ipsum Dolor</a></h3>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac lorem non lectus varius vulputate. 
+		Donec eleifend porta pretium. Pellentesque eu purus non lorem sagittis fringilla vel ut arcu. Ut fermentum 
+		vehicula facilisis. Aliquam bibendum ultricies ipsum, porttitor hendrerit justo sollicitudin vitae. 
+		Curabitur accumsan euismod arcu vitae volutpat. Class aptent taciti sociosqu ad litora torquent per conubia 
+		nostra, per inceptos himenaeos. Mauris nunc sapien, varius quis mattis non, condimentum suscipit massa. 
+		Nunc a nulla ut orci vestibulum suscipit. Pellentesque habitant morbi tristique senectus et netus et 
+		malesuada fames ac turpis egestas.
+	</p>
+	<ul>
+		<li><a href="">Lorem</a></li>
+		<li><a href="">Ipsum</a></li>
+		<li><a href="">Dolor</a></li>
+		<li><a href="">Sit</a></li>
+		<li><a href="">Amet</a></li>
+	</ul>
+</div>
 <?php
 	echo Layout::footer();
 ?>
--- work.php
+++ work.php
@@ -4,6 +4,54 @@
 	require_once("includes/layout.php");
 	echo Layout::header("Work");
 ?>
+<div>
+	<h3>Lorem Ipsum Dolor</h3>
+	<img src=""/>
+</div>
+<div>
+	<h3>Lorem Ipsum Dolor</h3>
+	<img src=""/>
+</div>
+<div>
+	<h3>Lorem Ipsum Dolor</h3>
+	<img src=""/>
+</div>
+<div>
+	<h3>Lorem Ipsum Dolor</h3>
+	<img src=""/>
+</div>
+<div>
+	<h3>Lorem Ipsum Dolor</h3>
+	<img src=""/>
+</div>
+<div>
+	<h3>Lorem Ipsum Dolor</h3>
+	<img src=""/>
+</div>
+<div>
+	<h3>Lorem Ipsum Dolor</h3>
+	<img src=""/>
+</div>
+<div>
+	<h3>Lorem Ipsum Dolor</h3>
+	<img src=""/>
+</div>
+<div>
+	<h3>Lorem Ipsum Dolor</h3>
+	<img src=""/>
+</div>
+<div>
+	<h3>Lorem Ipsum Dolor</h3>
+	<img src=""/>
+</div>
+<div>
+	<h3>Lorem Ipsum Dolor</h3>
+	<img src=""/>
+</div>
+<div>
+	<h3>Lorem Ipsum Dolor</h3>
+	<img src=""/>
+</div>
 
 <?php
 	echo Layout::footer();
