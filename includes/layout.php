b2c7f6cec34286c7e735c0942a4133e5868b6349
mclean.cw <havok2905@gmail.com>
2012-12-19 12:34:42 -0600

    added inlinelist class to css file and applied it to newly created social media icon list in the layout class

--- includes/layout.php
+++ includes/layout.php
@@ -63,7 +63,7 @@ EOT;
 				<div class="contact_container">
 					<div class="contact_sub_container">
 						<h4>Follow Me</h4>
-						<ul class="footertext">
+						<ul class="footertext inlinelist">
 							<li><a href="https://plus.google.com/u/0/108287038105459970762/posts"><img src="img/googlethumb.png"/><span>google+</span></a></li>
 							<li><a href="https://github.com/havok2905"><img src="img/gitthumb.png"/><span>github</span></a></li>
 							<li><a href="http://www.linkedin.com/pub/christopher-mclean/23/261/789"><img src="img/linkedthumb.png"></img><span>linkedin</span></a></li>
