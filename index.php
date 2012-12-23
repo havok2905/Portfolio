386ad185db38cfc0ec2d601971911d75955cc048
mclean.cw <havok2905@gmail.com>
2012-12-23 13:10:22 -0600

    modified png files for the icons to support universal scaling in the responsive site. Added a business card comp to be used in the projects page and started iconography for the navigation bar to be used in active and hover events.

--- index.php
+++ index.php
@@ -4,56 +4,14 @@
 	require_once("includes/layout.php");
 	echo Layout::header("Home");
 ?>
-
-<div>
-	<img src="">
-</div>
-<div>
-	<h3>What I Can Do</h3>
-	<div class="left">
-		<a href="">
-			<img src="img/laptop.png"/>
-			<h4>DEVELOPMENT</h4>
-		</a>
-		<ul class="seriftext">
-			<li>HTML, JavaScript, CSS3</li>
-			<li>PHP &amp; Fuel PHP Framework</li>
-			<li>Database Architecture</li>
-		</ul>
-	</div>
-	<div class="left">
-		<a href="">
-			<img src="img/phone.png"/>
-			<h4>MOBILE</h4>
-		</a>
-		<ul class="seriftext">
-			<li>Android Development</li>
-			<li>Mobile Web Development</li>
-			<li>Responsive Design</li>
-		</ul>
-	</div>
-	<div class="left">	
-		<a href="">
-			<img src="img/design.png"/>
-			<h4>DESIGN</h4>
-		</a>
-		<ul class="seriftext">
-			<li>Web &amp; Mobile Layouts</li>
-			<li>Logo Design &amp; Web Graphics</li>
-			<li>3D Modeling &amp; Rendering</li>
-		</ul>
-	</div>
-	<div class="left">	
-		<a href="">
-			<img src="img/graph.png"/>
-			<h4>ITERACTIVITY</h4>
-		</a>
-		<ul class="seriftext">
-			<li>Gamification</li>
-			<li>Social Media Integration</li>
-			<li>UI Design</li>
-		</ul>
-	</div>
+<h3>What I Can Do</h3>
+<div class="center_container">
+	<ul class="left">
+		<li><a href=""><img src="img/laptop.png"/><p>Development</p></a></li>
+		<li><a href=""><img src="img/phone.png"/><p>Mobile</p></a></li>
+		<li><a href=""><img src="img/design.png"/><p>Design</p></a></li>
+		<li><a href=""><img src="img/graph.png"/><p>Interactivity</p></a></li>
+	</ul>
 	<div class="clear"></div>
 </div>
 <div>
